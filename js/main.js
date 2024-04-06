7163c7cd38ec34546201de58f6e3b94d6d7c6b6d
Beatrice Ignacio <bignacio@myseneca.ca>
2024-04-06 00:27:38 -0400

    Headless CSS
    Added JS and JSON.

---
+++ js/main.js
@@ -0,0 +1,29 @@
+// const company_info = {
+//     "company_name": "Some Good Company",
+//     "owner": "Amanda Lutz",
+//     "address": "Toronto",
+//     "phone": "",
+//     "website": "",
+//     "socials": [
+//     {
+//     "name": "facebook",
+//     "handle": ""
+//     },
+//     { "name": "twitter", "url": "" },
+//     { "name": "instagram", "url": "" }
+//     ],
+//     "employees": [
+//     { "firstName": "Bob", "lastName": "Baker", "id": "5754" },
+//     { "firstName": "Susan", "lastName": "Smith", "id": "9764" },
+//     { "firstName": "Malcolm", "lastName": "Lutz", "id": "3456" }
+//     ]
+//     };
+
+//     console.log('company info: ', company_info);
+
+//     let result = company_info.employees[1].lastName;
+
+//     const lName = document.getElementById('demo');
+//     lName.innerHTML = result;
+
+console.log("hello");
